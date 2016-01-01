12694923494b5acba9edeeb5cc1a1def8f79c73b
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2016-01-01 15:07:41 +0100

    move context initialization to special function

--- include/mv.h
+++ include/mv.h
@@ -20,7 +20,24 @@ extern "C" {
 
 typedef struct mv_context mv_context;
 
+typedef enum mv_error {
+    /**
+     * No error happened (used to signal successful operation).
+     * Keep in mind that many API functions returning error codes can also
+     * return positive values, which also indicate success. API users can
+     * hardcode the fact that ">= 0" means success.
+     */
+    MV_ERROR_SUCCESS = 0,
+
+    /**
+     * Could not initialize a valid OpenCL context
+     */
+    MV_ERROR_OPENCL_INIT_FAILED = -1,
+} mv_error;
+
 mv_context *mv_context_create(void);
+int mv_context_initialize(mv_context *);
+void mv_context_free(mv_context *);
 void mv_clinfo(void);
 
 #ifdef __cplusplus
--- src/mv.c
+++ src/mv.c
@@ -2,9 +2,32 @@
 #include "ta.h"
 
 typedef struct mv_context {
+    cl_context cl_ctx;
 } mv_context;
 
-mv_context *mv_context_create(void) {
-    mv_context *ctx = ta_znew(NULL, mv_context);
+void mv_context_destructor(void *vctx)
+{
+    mv_context *ctx = vctx;
+    clReleaseContext(ctx->cl_ctx);
+}
+
+mv_context *mv_context_create(void)
+{
+    mv_context *ctx = ta_xznew(NULL, mv_context);
     return ctx;
+}
+
+int mv_context_initialize(mv_context *ctx)
+{
+    const cl_device_type type = CL_DEVICE_TYPE_GPU;
+    ctx->cl_ctx = clCreateContextFromType(NULL, type, NULL, NULL, NULL);
+    if (!ctx->cl_ctx)
+        return MV_ERROR_OPENCL_INIT_FAILED;
+    ta_set_destructor(ctx, mv_context_destructor);
+    return MV_ERROR_SUCCESS;
+}
+
+void mv_context_free(mv_context *ctx)
+{
+    ta_free(ctx);
 }
